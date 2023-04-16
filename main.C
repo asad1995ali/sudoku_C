e35bfd1e2b682382a37d674cfd4f3898382b6828
asad1995ali <asad1995ali@hotmail.com>
2023-04-16 15:19:35 +0200

    first plan

---
+++ main.C
@@ -0,0 +1,22 @@
+// Created by: Syed Asad Ali
+// Start Date: 16-04-2023
+
+/*
+This is how the program will function (or, the code that needs to be written)
+
+
+1. Create conversion functions based on measurement type (for eg. distance, speed, time, temperature) 
+2. Create table subfunctions based on different units (for eg. mile to km, mph to  kmh)
+3. Ask user for their measurement unit
+    3.1 Display table with available class options (distance, speed, time or temperature)
+    3.2 After input display the available units from that specific class of measurement
+    3.3 Ask user to input number corresponding to the unit
+4. Ask user to input value for their measurement 
+5. Ask user for conversion type
+    5.1 Display table with available units for that specific class, except the unit for their value which they specified in 3.2
+    5.2 Ask user to input the number corresponding to the conversion
+6. Call respective function and display the output 
+*/
+
+
+
